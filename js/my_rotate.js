384dcdae2c9e3c37fa1bd6ce63200e24b43ff15d
Matt Stillwell <matt.stillwell@blacklynx.tech>
2023-07-14 10:54:57 -0400

    :truck: Refactor JS

--- js/my_rotate.js
+++ js/my_rotate.js
@@ -1,34 +1,57 @@
+// Global variables
 const rotateWordSpan = document.getElementById('rotate-word');
 const words = ['iOS-App', 'Website', 'Game', 'Back-end', 'Software'];
-let currentWordIndex = 0;
 const colors = ['#BF616A', '#D08770', '#EBCB8B', '#A3BE8C', '#B48EAD']
+let currentWordIndex = 0;
 
-async function changeWord() {
+
+// Changes the currently shown word
+const changeWord = async() => {
     let currentWord = words[currentWordIndex];
     let currentColor = colors[currentWordIndex];
+
     rotateWordSpan.style.color = currentColor;
     rotateWordSpan.innerText = "";
+
+    // type in word, one letter at a time
     for(var i = 0; i < currentWord.length; i++) {
         let currentLetter = currentWord[i];
         rotateWordSpan.innerText += currentLetter;
+        // time to wait after adding a letter
         await sleep(200);
     }
+
+    // show full word time
     await sleep(500);
+
+    // remove letters from word
     for(var i = 0; i < currentWord.length; i++) {
         let currentWord = rotateWordSpan.innerText;
         let trimWord = currentWord.substring(0, currentWord.length - 1);
         console.log(currentWord, trimWord)
         rotateWordSpan.innerText = trimWord;
-        await sleep(100);
+        // time to wait after removing a letter
+        await sleep(200);
     }
+
+    // time to show no word
     await sleep(200);
-    // rotateWordSpan.innerText = currentWord;
+
+    // move index to next word
     currentWordIndex = currentWordIndex < words.length - 1 ? currentWordIndex + 1 : 0;
-    changeWord();
+
+    // call this method again
+    await changeWord();
 }
 
-function sleep(ms) {
+// waits for given number of milliseconds
+const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+// Auto run main method
+const main = async() => {
+    await changeWord();
 }
 
-changeWord();
+main();
